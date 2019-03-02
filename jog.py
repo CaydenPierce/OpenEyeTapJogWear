c3240ea37dbc8cc788d7efa789343305225eef2e
Cayden <cpierc3@uwo.ca>
2019-03-02 17:37:07 -0500

    distance function working, each jog new log added

--- jog.py
+++ jog.py
@@ -39,8 +39,8 @@ def createLog(name): #adds log of seeing person. Contains context such as who, w
 
             wr = csv.writer(log_csv, delimiter = ',')
             wr.writerow(memory)
-'''
-def distanceTwoPoints(point1, point2): #returns distance between two points, in meters
+
+def distanceTwoPoints(point1, point2): #returns distance between two points, in kilometers
 	# approximate radius of earth in km
 	R = 6373.0
 
@@ -55,16 +55,22 @@ def distanceTwoPoints(point1, point2): #returns distance between two points, in 
 	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
 	c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-	distance = R * c * 1000
+	distance = R * c
 	return distance
 
 def totalDistance(name):
-    with open(os.path.join(os.path.dirname(__file__), "memory/{}.csv".format(name)), "a", newline="") as joglog: #open in append and read mode
-        reader = csv.reader(joglog, delimiter = ',')
-	for row in reader[0:]:
-		print(row)
+	points = [] #list of points to find distances between
+	totalDistance = 0 #totalDistance travelled
 
-'''
+	with open(os.path.join(os.path.dirname(__file__), "memory/{}.csv".format(name)), "r") as joglog: #open in append and read mode
+		reader = csv.reader(joglog, delimiter = ',')
+		for i, row in enumerate(reader):
+			if ((i % 30) - 1 == 0): #only check every thirty coordinates
+				points.append([float(row[2]), float(row[3])]) #append coordinate point to points list
+	for i, point in enumerate(points[:-1]):
+		totalDistance += distanceTwoPoints(points[i], points[i+1])
+	return totalDistance
+
 
 #start the bluetooth server for GPS
 sock = GPSbluetooth.startBluetoothServer()
@@ -73,16 +79,24 @@ sock = GPSbluetooth.startBluetoothServer()
 name = sys.argv[1]
 
 #create file to save coordinates
-#createJogFile(name)
+createJogFile(name)
 
-while True: #main 
-	createLog(name) #create a new log with time, speed, and location data
-	sleep(1)
+try:
+	while True: #main 
+		createLog(name) #create a new log with time, speed, and location data
+		sleep(1)
+		'''with open(os.path.join(os.path.dirname(__file__), "memory/{}.csv".format(name)), "a", newline="") as log_csv: #open in append and read mode
+			wr = csv.writer(log_csv, delimiter = ',')
+			wr.writerow(["hello", "NA", "NA", "NA"])'''
 
-#find total distance of jog, append it to end of log
-#distance = jogReader(name)
+finally:
+	#find total distance of jog, append it to end of log
+	distance = totalDistance(name)
+	with open(os.path.join(os.path.dirname(__file__), "memory/{}.csv".format(name)), "a", newline="") as joglog: #open in append and read mode
+		joglog.write('\n')
+		joglog.write("Total distance was: {} km \n".format(distance))
 
-#clean up when complete
-sock.close()      
+	#clean up when complete
+	sock.close()   
 
 
